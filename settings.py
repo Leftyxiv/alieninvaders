03cef53abeee7f59e6451852087fbf27cd576e1d
Leftyxiv <manny@aledoux.net>
2021-03-17 18:09:43 -0400

    initial commit

---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings():
+  """
+  A class to store all of the settings
+  for my alien adventure game
+  """
+  
+  def __init__(self):
+    """
+    initaliaze the game's settings
+    """
+    self.screen_width = 1200
+    self.screen_height = 800
+    self.bg_color = (230,230,230)
---
+++ ship.py
@@ -0,0 +1,25 @@
+import pygame
+
+class Ship():
+  """
+  Class for the player's ship
+  """
+  def __init__(self, screen):
+    """
+    initialize the ship and set it's starting position
+    """
+    self.screen = screen
+
+    self.image = pygame.image.load('images/ship.bmp')
+    self.rect = self.image.get_rect()
+    self.screen_rect = scree.get_rect()
+
+    # start each ship at the same place
+    self.rect.centerx = self.screen_rect.centerx
+    self.rect.bottom = self.screen_rect.bottom
+  
+  def blitme(self):
+    """
+    draw the ship at it's current location
+    """
+    self.screen.blit(self.image, self.rect)
