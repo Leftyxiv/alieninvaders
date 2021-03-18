2383f52c82ca9cc52ecc7092be535781e48b7e31
Leftyxiv <manny@aledoux.net>
2021-03-18 11:48:57 -0400

    alien fleet now renders to the screen

--- game_functions.py
+++ game_functions.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 from bullet import Bullet
+from alien import Alien
 
 def check_events(ai_settings, screen, ship, bullets):
   """Respond to keypresses and mouse events"""
@@ -39,7 +40,7 @@ def check_keyup_events(event, ai_settings, screen, ship, bullets):
 
 
 
-def update_screen(ai_settings, screen, ship, alien, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
   """Update the images on the screen and flip to a new screen"""
   screen.fill(ai_settings.bg_color)
 
@@ -49,7 +50,7 @@ def update_screen(ai_settings, screen, ship, alien, bullets):
 
   # draw a ship on the screen
   ship.blitme()
-  alien.blitme()
+  aliens.draw(screen)
 
   pygame.display.flip()
 
@@ -67,3 +68,17 @@ def fire_bullet(ai_settings, screen, ship, bullets):
   if len(bullets) < ai_settings.bullets_allowed:
       new_bullet = Bullet(ai_settings, screen, ship)
       bullets.add(new_bullet)
+
+def create_fleet(ai_settings, screen, aliens):
+  """create a full fleet of aliens"""
+  alien = Alien(ai_settings, screen)
+  alien_width = alien.rect.width
+  available_space_x = ai_settings.screen_width - 2 * alien_width
+  number_aliens_x = int(available_space_x / (2 * alien_width))
+
+  # create the first row of alien invaders
+  for alien_number in range(number_aliens_x):
+    alien = Alien(ai_settings, screen)
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
