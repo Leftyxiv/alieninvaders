5717da71dee522359d95c2f0f615363e8afe5460
Leftyxiv <manny@aledoux.net>
2021-03-19 07:56:15 -0400

    renders an entire alien fleet to the screen now

--- game_functions.py
+++ game_functions.py
@@ -69,16 +69,35 @@ def fire_bullet(ai_settings, screen, ship, bullets):
       new_bullet = Bullet(ai_settings, screen, ship)
       bullets.add(new_bullet)
 
-def create_fleet(ai_settings, screen, aliens):
+def create_fleet(ai_settings, screen, ship, aliens):
   """create a full fleet of aliens"""
   alien = Alien(ai_settings, screen)
-  alien_width = alien.rect.width
+  number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+  num_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
+
+  # create the fleet of alien invaders
+  for row in range(num_rows):
+    for alien_number in range(number_aliens_x):
+      print(row, alien_number)
+      create_alien(ai_settings, screen, aliens, alien_number, row)
+
+def get_number_aliens_x(ai_settings, alien_width):
+  """Determine the number of aliens that fit in a row"""
   available_space_x = ai_settings.screen_width - 2 * alien_width
   number_aliens_x = int(available_space_x / (2 * alien_width))
+  return number_aliens_x
 
-  # create the first row of alien invaders
-  for alien_number in range(number_aliens_x):
-    alien = Alien(ai_settings, screen)
-    alien.x = alien_width + 2 * alien_width * alien_number
-    alien.rect.x = alien.x
-    aliens.add(alien)
+def create_alien(ai_settings, screen, aliens, alien_number, row_number):
+  """Create an alien and place it in the row"""
+  alien = Alien(ai_settings, screen)
+  alien_width = alien.rect.width
+  alien.x = alien_width + 2 * alien_width * alien_number
+  alien.rect.x = alien.x
+  alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+  aliens.add(alien)
+
+def get_number_rows(ai_settings, ship_height, alien_height):
+  """Determine the number of rows of aliens that can fit on the screen"""
+  available_space_y = (ai_settings.screen_height - (3 * alien_height) - ship_height)
+  num_rows = int(available_space_y / (2 * alien_height))
+  return num_rows
